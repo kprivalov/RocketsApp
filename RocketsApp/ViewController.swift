7a211377545fa2a49efcaecd70a16839a345d031
kirill-privalov <kirill.privalov.mail@gmail.com>
2022-06-21 18:43:14 +0300

    Initial Commit

---
+++ RocketsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RocketsApp
+//
+//  Created by Kirill Privalov on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
